0adb667662bc7f9163b274caab93e333109da251
Vikhyath Choradia <vikhyatc@thoughtworks.com>
2025-06-25 12:20:15 +0200

    Update service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
+const cacheName = "romantic-countdown-v1";
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./widget.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll(["/index.html.", "/widget.html", "/manifest.json"]);
+    caches.open(cacheName).then((cache) => {
+      return Promise.all(
+        filesToCache.map((url) =>
+          fetch(url).then((response) => {
+            if (!response.ok) throw new Error("Failed to fetch " + url);
+            return cache.put(url, response.clone());
+          })
+        )
+      );
     })
   );
 });
