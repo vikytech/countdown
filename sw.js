7d9a87b6a6eff0d5eaaf3bedfe771e366eb895cf
Vikhyath Choradia <vikhyatc@thoughtworks.com>
2025-06-26 00:50:59 +0200

    Add notification with images

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "romantic-countdown-v3";
+const cacheName = "romantic-countdown-v4";
 const filesToCache = [
   "./index.html",
   "./manifest.json",
@@ -7,6 +7,8 @@ const filesToCache = [
 ];
 
 self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+  self.skipWaiting();
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
         caches.delete
@@ -27,5 +29,42 @@ self.addEventListener("fetch", (event) => {
     caches
       .match(event.request)
       .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("push", (event) => {
+  const data = event.data?.json() || {};
+  const title = data.title || "Countdown Update!";
+  const options = {
+    body:
+      data.body ||
+      "Time is flying! See how close you are to meeting your love 💖",
+    icon: "/icons/icon-192.png",
+    badge: "/icons/badge-72.png",
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const targetUrl =
+    event.notification.data?.url || "https://countdown.vikytech.in";
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((windowClients) => {
+        for (let client of windowClients) {
+          if (client.url === targetUrl && "focus" in client)
+            return client.focus();
+        }
+        if (clients.openWindow) return clients.openWindow(targetUrl);
+      })
   );
 });
